bf4f7785829ca41d12190d9dc3fd4bf4d0c1d993
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2025-07-03 16:41:19 +0900

    feat: テスト環境の設定を改善
    - GitHub ActionsのワークフローにXvfbのセットアップを追加し、テスト実行時に表示環境を設定
    - テスト実行コマンドを修正し、xvfb-runを使用してテストを実行
    - package.jsonのテストスクリプトに設定ファイルを追加
    - 新しいvscode-test.config.jsファイルを作成し、テスト設定を定義

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,16 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
-
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
+// 基本的なユニットテスト
+suite('Basic Test Suite', () => {
+	test('配列インデックステスト', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		assert.strictEqual(1, [1, 2, 3].indexOf(2));
+	});
+
+	test('文字列操作テスト', () => {
+		assert.strictEqual('hello', 'hello');
+		assert.strictEqual('hello world'.split(' ')[0], 'hello');
+		assert.strictEqual('hello world'.split(' ')[1], 'world');
 	});
 });
